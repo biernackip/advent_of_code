6712e622eeae381b8e09eb8dc6c4234714f0a761
Pawel Biernacki <biernackip@yahoo.com>
2018-12-17 12:20:33 -0500

    Finished 09&10

---
+++ 2018/09.py
@@ -0,0 +1,51 @@
+import collections
+# input:
+n_marbles = 70851*100
+n_players = 477
+
+# test values:
+# n_marbles = 1618
+# n_players = 10
+# score = 8317
+
+# n_marbles = 7999
+# n_players = 13
+# score = 146373
+
+# n_marbles = 1104
+# n_players = 17
+# score = 2764
+
+# n_marbles = 6111
+# n_players = 21
+# score = 54718
+
+# n_marbles = 5807
+# n_players = 30
+# score = 37305
+
+
+# n_marbles = 25
+# n_players = 9
+
+skip_marble = 23
+
+player_scores = [0 for x in range(n_players)]
+marble_array = []
+
+def play_game(max_players, last_marble):
+    scores = collections.defaultdict(int)
+    circle = collections.deque([0])
+
+    for marble in range(1, last_marble + 1):
+        if marble % skip_marble == 0:
+            circle.rotate(7)
+            scores[marble % max_players] += marble + circle.pop()
+            circle.rotate(-1)
+        else:
+            circle.rotate(-1)
+            circle.append(marble)
+
+    return max(scores.values()) if scores else 0
+
+print(play_game(n_players, n_marbles))
---
+++ 2018/10.py
@@ -0,0 +1,42 @@
+from matplotlib import pyplot as plt
+import numpy as np
+
+x = []
+y = []
+vx = []
+vy = []
+
+with open('input10.txt') as f:
+# with open('test_input10.txt') as f:
+    lines = f.readlines()
+
+
+# split lines
+for line in lines:
+    x.append(int(line.split(',')[0].split('<')[-1]))
+    y.append(int(line.split(',')[1].split('>')[0]))
+    vx.append(int(line.split('velocity=<')[1].split(',')[0]))
+    vy.append(int(line.split('velocity=<')[1].split(',')[1].split('>')[0]))
+
+# turn into numpy arrays
+x = np.array(x)
+y = np.array(y)
+vx = np.array(vx)
+vy = np.array(vy)
+
+# let's minimize distance
+maxx = 10000
+best_t = 0
+
+for t in range(10300,10500):
+    xt = x+t*vx
+    yt = y+t*vy
+
+    if max(xt)-min(xt) < maxx:
+        maxx = max(xt)-min(xt)
+        best_t = t
+
+# viz
+plt.scatter(x+best_t*vx, y+best_t*vy, s=3)
+plt.savefig('viz_10.pdf')
+print(best_t)
